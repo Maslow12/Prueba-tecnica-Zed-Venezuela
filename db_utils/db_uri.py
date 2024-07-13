165ee38328ec43db1690b6f831b55f63e14f0049
Sebastian <40075601+Maslow12@users.noreply.github.com>
2024-07-13 13:18:45 -0400

    Elimino cache

--- db_utils/db_uri.py
+++ db_utils/db_uri.py
@@ -1,13 +1,3 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-host = os.getenv("MYSQL_HOST")
-port = 3306
-user = os.getenv("MYSQL_USER")
-password = os.getenv("MYSQL_PASSWORD")
-database = os.getenv("MYSQL_DATABASE")
 
 def mariadb_uri_2():
     uri = "mysql+pymysql://test_user:admin@db:3306/test_db"
